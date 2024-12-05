2545dc2782da13eb24ba07a69e4d4721558321b2
Yves De Saedeleer <ydsdsy@gmail.com>
2024-12-05 12:54:48 +0100

    Update Bird_Migration_Tool.py

--- Bird_Migration_Tool.py
+++ Bird_Migration_Tool.py
@@ -268,13 +268,12 @@ tabs = st.tabs(["Weergegevens", "Voorspellingen", "Under construction", "To be d
 
 # Functie voor het weergeven van de regels in een mooi formaat (zonder SVG, enkel tekst en iconen)
 def format_regel_with_icons(time, temperature, precipitation, cloud_cover, cloud_cover_low, cloud_cover_mid, cloud_cover_high, wind_direction, wind_speed_10m, wind_speed_80m, wind_speed_120m, wind_speed_180m, visibility):
-    return (
-        f"🕒:{time}|🌡️{temperature:.1f}°C|🌧️:{precipitation:.1f}mm|☁️Tot.:{cloud_cover}%|"
-        f"(☁️L:{cloud_cover_low}%|☁️M:{cloud_cover_mid}%|☁️H:{cloud_cover_high}%)"
-        f"🧭:{wind_direction}|💨@10m:{wind_speed_10m}Bf|💨@80m:{wind_speed_80m}Bf|"
-        f"💨@120m:{wind_speed_120m}Bf|💨@180m:{wind_speed_180m}Bf|👁️:{visibility:.1f}km"
-    )
-
+return (
+    f"🕒: {time:<5} | 🌡️ {temperature:>5.1f}°C | 🌧️ {precipitation:>5.1f}mm | ☁️ Tot.: {cloud_cover:>3}% | "
+    f"(☁️ L: {cloud_cover_low:>3}% | ☁️ M: {cloud_cover_mid:>3}% | ☁️ H: {cloud_cover_high:>3}%) "
+    f"🧭: {wind_direction:<2} | 💨 @10m: {wind_speed_10m:>2}Bf | 💨 @80m: {wind_speed_80m:>2}Bf | "
+    f"💨 @120m: {wind_speed_120m:>2}Bf | 💨 @180m: {wind_speed_180m:>2}Bf | 👁️ {visibility:>6.1f}km"
+)
 
 # Tab 0: Weergeven van de gegevens
 with tabs[0]: #dit is het meest linkse tabblad
