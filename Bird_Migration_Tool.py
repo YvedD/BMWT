61de92ba1b362e38a29e9a4b4879bb49ed91bab2
Yves De Saedeleer <ydsdsy@gmail.com>
2025-01-04 19:15:03 +0100

    Update Bird_Migration_Tool.py

--- Bird_Migration_Tool.py
+++ Bird_Migration_Tool.py
@@ -288,7 +288,7 @@ Gebruik de tabbladen hieronder om de gegevens te verkennen en aan te passen naar
 
 
 # Hoofdvenster met tabbladen
-tabs = st.tabs(["Weergegevens", "Voorspellingen", "Vliegbeelden", "CROW project", "BIRDTAM project", "Trektellen.nl (read only)", "Crane Radar", "Gebruiksaanwijzing"])
+tabs = st.tabs(["Weergegevens", "Voorspellingen", "Vliegbeelden", "Trektelmodule - geluiden", "CROW project", "BIRDTAM project", "Trektellen.nl (read only)", "Crane Radar", "Gebruiksaanwijzing"])
 
 
 # Tab 0: Weergeven van de gegevens
@@ -621,13 +621,20 @@ with tabs[2]:
         height=11000  # Hoogte van de iframe
     )
 with tabs[3]:
+    st.header("Trektelmodule - geluiden")
+    st.components.v1.iframe(
+        "https://www.natuurstudiegroepdijleland.be/trektelmodule-soortenfiches/",  # URL van de externe website
+        width=1000,  # Breedte van de iframe
+        height=11000  # Hoogte van de iframe
+    )
+with tabs[4]:
     st.header("CROW project")
     st.components.v1.iframe(
         "https://www.meteo.be/services/birdDetection/#/",  # URL van de externe website
         width=1000,  # Breedte van de iframe
         height=2000  # Hoogte van de iframe
     )
-with tabs[4]:
+with tabs[5]:
     st.header("BIRDTAM density profiles project")
     st.components.v1.iframe(
         "https://www.flysafe-birdtam.eu/profiles.html",  # URL van de externe website
@@ -635,7 +642,7 @@ with tabs[4]:
         height=2000  # Hoogte van de iframe
     )
 
-with tabs[5]:
+with tabs[6]:
     st.header("Trektellen.nl (read only)")
     st.components.v1.iframe(
         "https://www.trektellen.nl",  # URL van de externe website
@@ -643,7 +650,7 @@ with tabs[5]:
         height=2500  # Hoogte van de iframe
     )
 
-with tabs[6]:
+with tabs[7]:
     st.header("Kraanvogel radar)")
     st.components.v1.iframe(
         "https://analytical.sensingclues.org/cranes/",  # URL van de externe website
@@ -651,7 +658,7 @@ with tabs[6]:
         height=2500  # Hoogte van de iframe
     )
 
-with tabs[7]:
+with tabs[8]:
     st.header("Handleiding")
     # Eenvoudige handleiding
     st.text("""
