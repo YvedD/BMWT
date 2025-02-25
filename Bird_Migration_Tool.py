2be4a8d90caea6b419a2212a8021d729046ceeed
Yves De Saedeleer <ydsdsy@gmail.com>
2025-02-25 13:23:48 +0100

    Update Bird_Migration_Tool.py

--- Bird_Migration_Tool.py
+++ Bird_Migration_Tool.py
@@ -618,11 +618,11 @@ with tabs[1]:
         lon = st.session_state.get("lon", 4.768)   # Standaardwaarde als lon niet is ingesteld
 
         # Maak de dynamische URL
-        #          f"https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=6&overlay=wind&product=ecmwf&level=surface&lat={lat}&lon={lon}"
-        windy_url =f"https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=bft&zoom=6&overlay=wind&product=ecmwf&level=surface&lat={lat}&lon={lon}&detailLat={lat}&detailLon={lon}&detail=true&pressure=true"
+        windy_url =f"https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=6&overlay=wind&product=ecmwf&level=surface&lat={lat}&lon={lon}"
+       #windy_url =f"https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=bft&zoom=6&overlay=wind&product=ecmwf&level=surface&lat={lat}&lon={lon}&detailLat={lat}&detailLon={lon}&detail=true&pressure=true"
         st.markdown(
             f"""
-            <iframe width="100%" height="100%" src="{windy_url}" frameborder="0"></iframe>
+            <iframe width="100%" height="650%" src="{windy_url}" frameborder="0"></iframe>
             """,
             unsafe_allow_html=True
         )
