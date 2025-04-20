85736ee88a2bedef9ee6e32babf2186fac673a57
Yves De Saedeleer <ydsdsy@gmail.com>
2025-04-20 21:05:39 +0200

    Update soorten_geluiden.py

--- soorten_geluiden.py
+++ soorten_geluiden.py
@@ -15,5 +15,21 @@ iframe_data = {
         "https://xeno-canto.org/977679/embed",
         "https://xeno-canto.org/977678/embed"
     ]
+    "Waterpieper": [
+        "https://xeno-canto.org/982396/embed",
+        "https://xeno-canto.org/977038/embed",
+        "https://xeno-canto.org/975199/embed",
+        "https://xeno-canto.org/953880/embed",
+        "https://xeno-canto.org/953464/embed",
+        "https://xeno-canto.org/948886/embed"
+    ],
+    "Oeverpieper": [
+        "https://xeno-canto.org/984474/embed",
+        "https://xeno-canto.org/984466/embed",
+        "https://xeno-canto.org/979676/embed",
+        "https://xeno-canto.org/979675/embed",
+        "https://xeno-canto.org/953436/embed",
+        "https://xeno-canto.org/943481/embed"
+    ]
     # Voeg hier zelf nieuwe soorten toe
 }
