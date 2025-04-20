efd85e744ffdb0c836ea7e62362e3e7d5b93600b
Yves De Saedeleer <ydsdsy@gmail.com>
2025-04-20 21:37:01 +0200

    Update soorten_geluiden.py

--- soorten_geluiden.py
+++ soorten_geluiden.py
@@ -23,13 +23,37 @@ iframe_data = {
         "https://xeno-canto.org/953464/embed",
         "https://xeno-canto.org/948886/embed"
     ],
-    "Oeverpieper": [
-        "https://xeno-canto.org/984474/embed",
-        "https://xeno-canto.org/984466/embed",
-        "https://xeno-canto.org/979676/embed",
-        "https://xeno-canto.org/979675/embed",
-        "https://xeno-canto.org/953436/embed",
-        "https://xeno-canto.org/943481/embed"
+    "Mongoolse pieper": [
+        "https://xeno-canto.org/942303/embed",
+        "https://xeno-canto.org/926679/embed",
+        "https://xeno-canto.org/835671/embed",
+        "https://xeno-canto.org/833846/embed",
+        "https://xeno-canto.org/766183/embed",
+        "https://xeno-canto.org/766182/embed"
+    ],
+    "Grote pieper": [
+        "https://xeno-canto.org/959054/embed",
+        "https://xeno-canto.org/958766/embed",
+        "https://xeno-canto.org/957973/embed",
+        "https://xeno-canto.org/957101/embed",
+        "https://xeno-canto.org/956732/embed",
+        "https://xeno-canto.org/950295/embed"
+    ],
+    "Duinieper": [
+        "https://xeno-canto.org/951316/embed",
+        "https://xeno-canto.org/951315/embed",
+        "https://xeno-canto.org/951314/embed",
+        "https://xeno-canto.org/951313/embed",
+        "https://xeno-canto.org/951281/embed",
+        "https://xeno-canto.org/828895/embed"
+    ],
+    "Siberische boompieper": [
+        "https://xeno-canto.org/835677/embed",
+        "https://xeno-canto.org/766271/embed",
+        "https://xeno-canto.org/765805/embed",
+        "https://xeno-canto.org/759347/embed",
+        "https://xeno-canto.org/759346/embed",
+        "https://xeno-canto.org/757598/embed"
     ]
     # Voeg hier zelf nieuwe soorten toe 
 }
